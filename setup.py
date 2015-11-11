d31ce5dc99ea6de7d2272be20b4adcc0190aa02b
almond55 <exileam@yahoo.com>
2015-11-11 11:54:36 +0900

    Updated xoxzo's info in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="xoxzo.recipe.template",
     version="1.2.2",
-    author="Abdul Rahman Ibrahim",
-    author_email="rahman@xoxzo.com",
+    author="xoxzo.com",
+    author_email="engineering@xoxzo.com",
     url="https://github.com/xoxzo/amplecode.recipe.template",
     description="Buildout recipe for making files out of Jinja2 templates",
     long_description=open("README.rst").read(),
@@ -20,11 +20,11 @@ setup(
     keywords="zc.buildout recipe template Jinja2",
     license="BSD",
     packages=find_packages(),
-    namespace_packages=("brodul", "brodul.recipe"),
+    namespace_packages=("xoxzo", "xoxzo.recipe"),
     install_requires=("setuptools", "zc.recipe.egg", "Jinja2", "zope.dottedname"),
     zip_safe=True,
     entry_points="""
         [zc.buildout]
-        default = brodul.recipe.template:Recipe
+        default = xoxzo.recipe.template:Recipe
     """,
 )
